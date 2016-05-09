0771ade3598b53d99797f62c97c2c2a63c5bb210
Kurtis Lloyd <klloyd@student.neumont.edu>
2016-05-09 17:46:32 -0600

    Added mustache implimation

--- GifHub/src/main/java/Main/Main.java
+++ GifHub/src/main/java/Main/Main.java
@@ -1,21 +1,43 @@
 package Main; /**
  * Created by Kurtis Lloyd on 5/3/2016.
  */
+import java.io.File;
+import java.io.FileWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.rmi.server.ExportException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.Version;
+import jdk.nashorn.internal.ir.RuntimeNode;
+import spark.ModelAndView;
+import spark.Request;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import static spark.Spark.*;
+
 public class Main {
 
     public static void main(String [] args){
-        port(Integer.valueOf(System.getenv("PORT")));
-        String startingString = "Welcome to HEROKU THING I DID IT!";
-        get("home", (req, res) -> startingString);
+        HTML html = new HTML();
+        port(8080);
+       //port(Integer.valueOf(System.getenv("PORT")));
+       // String startingString = "Welcome to HEROKU THING I DID IT!";
+       // get("home", (req, res) -> startingString);
+        Map map = new HashMap<>();
+        map.put("name", "Kurtis");
+        map.put("object", "Object");
+        get("/hello", ((request, response) -> new ModelAndView(map,"Templates/hello.mustache")), new MustacheTemplateEngine());
+    }
 
 
-    }
-    public static int getHerokuAssignedPort(){
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if(processBuilder.environment().get("PORT") != null){
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567;
-    }
 }
---
+++ GifHub/src/main/java/Main/User.java
@@ -0,0 +1,10 @@
+package Main;
+
+/**
+ * Created by Kurtis Lloyd on 5/9/2016.
+ */
+public class User {
+    private int id;
+    private String userName;
+    private String password;
+}
