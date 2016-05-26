db935b8fa92128e35aefea0ad77e473fd22fb5fd
Kurtis Lloyd <klloyd@student.neumont.edu>
2016-05-25 19:54:30 -0600

    Ignore .idea, and target.

--- GifHub/src/main/java/Main/Main.java
+++ GifHub/src/main/java/Main/Main.java
@@ -3,6 +3,7 @@ package Main; /**
  */
 import java.util.*;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
@@ -11,7 +12,9 @@ import javax.management.Query;
 
 import static spark.Spark.*;
 
+
 public class Main {
+   static String FinalUsername;
 
     public static void main(String [] args){
         port(8082);
@@ -28,6 +31,8 @@ public class Main {
         }
         final User endEnduser = EndUser;
 
+
+
        get("/", (request, response) -> {
            Map model = new HashMap();
            model.put("Username", endEnduser.getUserName());
@@ -43,31 +48,70 @@ public class Main {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+
+
         get("/form", (request, response) -> {
             Map model = new HashMap();
             model.put("template", "form.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
-//
-//      hibernateQuery.makeTransaction();
-//
-//        User user = new User();
-//        user.setId(3);
-//        user.setPassword("ThisTestPasswordAgain");
-//        user.setUserName("SecondTestUsernameAgain");
-//
-//        hibernateQuery.getSession().save(user);
-//        hibernateQuery.getSession().getTransaction().commit();
-//        hibernateQuery.endTrasaction();
 
+        get("/detector", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "detector.vtl");
+            writeUserToDatabase(request.queryParams("Username"));
+            String Username =  getUsername(request.queryParams("Username"));
+            boolean isUsername =  loginUser(Username);
 
-//      Map map = new HashMap<>();
-//        map.put("Username", "Grab from ORM database");
-//        map.put("id", "UserID");
-
-//        get("/hello", ((request, response) -> new ModelAndView(map,"Templates/hello.vtl")), new MustacheTemplateEngine());
-      //  get("/login", (((request, response) -> new ModelAndView(map,"Templates/login.vtl"))), new MustacheTemplateEngine());
-
+            model.put("loginUser", isUsername);
+            model.put("Username", Username);
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
     }
-}
+
+
+    public static Boolean loginUser(String username) {
+
+        writeUserToDatabase(username);
+        HibernateQuery hibernateQuery = new HibernateQuery();
+        List<User> userList = hibernateQuery.writeQuery("From User E where E.Username =" + username);
+        User user = new User();
+        for (User u : userList
+                ) {
+            user = u;
+        }
+        final User finalUser = user;
+
+
+        if (username.equals(finalUser.getUserName())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static String getUsername(String username) {
+        HibernateQuery hibernateQuery = new HibernateQuery();
+        List<User> userList = hibernateQuery.writeQuery("From User E where E.Username =" + username);
+        User user = new User();
+        for (User u : userList
+                ) {
+            user = u;
+        }
+        final User finalUser = user;
+
+        return finalUser.getUserName();
+    }
+        public static void writeUserToDatabase(String username) {
+        User user = new User();
+            user.setUserName(username);
+
+            HibernateQuery hibernate = new HibernateQuery();
+            hibernate.getSession().save(user);
+            hibernate.getSession().getTransaction().commit();
+            hibernate.endTrasaction();
+
+    }
+
+    }
+
