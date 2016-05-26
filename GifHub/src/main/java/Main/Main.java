7d46028e14d2229ff737877e00cb21e80174a3bd
Kurtis Lloyd <klloyd@student.neumont.edu>
2016-05-25 18:33:28 -0600

    Added Velocity form and was able to query the database and display on an html page

--- GifHub/src/main/java/Main/Main.java
+++ GifHub/src/main/java/Main/Main.java
@@ -7,6 +7,8 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 
+import javax.management.Query;
+
 import static spark.Spark.*;
 
 public class Main {
@@ -14,18 +16,35 @@ public class Main {
     public static void main(String [] args){
         port(8082);
         String layout = "layout.vtl";
+        List<User> queryResults = new ArrayList<>();
 
-      // HibernateQuery hibernateQuery = new HibernateQuery();
+       HibernateQuery hibernateQuery = new HibernateQuery();
+       queryResults = hibernateQuery.writeQuery("FROM User E WHERE E.id = 5");
+            User EndUser = new User();
+        for (User u: queryResults
+             ) {
+            EndUser = u;
+        }
+        final User endEnduser = EndUser;
 
        get("/", (request, response) -> {
            Map model = new HashMap();
+           model.put("Username", endEnduser.getUserName());
+           model.put("Password", endEnduser.getPassword());
            model.put("template", "hello.vtl");
+
            return new ModelAndView(model, layout);
        }, new VelocityTemplateEngine());
 
         get("/login", (request, response) -> {
         Map model = new HashMap();
             model.put("template", "login.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/form", (request, response) -> {
+            Map model = new HashMap();
+            model.put("template", "form.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 //
